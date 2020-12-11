506bf4414e9c059eb9d6e7c719f8b5699c761ce1
saharVac <vcsahar@gmail.com>
2020-12-11 11:18:29 -0500

    fixed main page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -9,33 +9,28 @@ function homePage() {
 
             <div className="col-sm-1"></div>
 
-            <div className="col-sm-10">
+            <div className="main-container col-sm-10">
+            <div className="about-title">
+                <h1>About Me</h1>
+            </div>
             <div className="row">
                 <div className="col-2 col-sm-1"></div>
                 <div className="col-10 col-sm-6 col-md-5">
+                <div id="title" className="col-9 col-sm-4 col-md-5" >
+                    
+                </div>
+                <div className="col-3 col-sm-1"></div>
                 <img
                     src={headshot}
                     alt="headshot"
                     width="200px"
                 />
                 </div>
-                <div className="col-3 col-sm-1"></div>
-                <div id="title" className="col-9 col-sm-4 col-md-5" >
-                    <div className="row">
-                        <h1>About</h1>
-                    </div>
-                    <div className="row">
-                        <h1>Me</h1>
-                    </div>
-                </div>
             </div>
-            <div className="row mt-4 pt-4">
+            <div className="about-text row mt-4 pt-4">
                 <p>
-                After graduating in from University of Binghamton with
-                a Bachelor of Science in Electrical Engineering, I am now finishing Columbia University's Web Development program.
-                </p>
-                <p>
-                I am now pursuing a career in Web Development, seeking employment as a developer.
+                    A dedicated, results-oriented web developer curious with conceptualizing ideas to translate into action. Graduated from Binghamton University’s Electrical Engineering program and currently finishing up Columbia University’s Full Stack Web Development Certification. Looking to leverage my passion for responsive web design, experience in collaborative team work, ability to maintain focus in a fast-paced environment, and possibly my written and oral fluency in Hebrew.
+
                 </p>
             </div>
             </div>
