4e431de93c7ad3730c49b397f4bc8ef4f06afc63
saharVac <vcsahar@gmail.com>
2020-12-15 11:48:24 -0500

    Styled contact page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,51 +1,64 @@
 import React from "react"
 
 function Contact() {
+
+    const sendMessage = () => {
+        alert("Message Sent!")
+    }
      
     return (
         <main className="contact-main row">
             <div className="container col-12">
                
-                        <h1>Contact Me</h1>
-                        <form className="mt-4 pt-4" >
-                            <div className="form-group">
-                                <label htmlFor="name">Full Name</label>
-                                <input
-                                type="email"
-                                className="form-control"
-                                id="name"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email address</label>
-                                <input
-                                type="email"
-                                className="form-control"
-                                id="email"
-                                placeholder="name@example.com"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="exampleFormControlTextarea1">Message</label>
-                                <textarea
-                                className="form-control"
-                                id="exampleFormControlTextarea1"
-                                rows="3"
-                                ></textarea>
-                            </div>
-                            <button type="submit">Submit</button>
-                        </form>
-                        <section id="personal-info">
-                            <p>
-                                <i className="fas fa-phone"></i> +1 (516) 713 4111
-                            </p>
-                            <p>
-                                <i className="fas fa-envelope-square"></i> vcsahar@gmail.com
-                            </p> 
-                        </section>
-                        
+                <h1>Contact Me</h1>
+                <form className="contact-form mt-4 pt-4" >
+                    <div className="form-group">
+                        <label htmlFor="name">Full Name</label>
+                        <input
+                        type="email"
+                        className="form-control"
+                        id="name"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        placeholder="name@example.com"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlTextarea1">Message</label>
+                        <textarea
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"
+                        ></textarea>
+                    </div>
+                    <center>
+                    <button 
+                    type="submit" 
+                    onClick={(e) => {
+                        e.preventDefault()
+                        sendMessage()
+                    }}>
+                        Submit
+                    </button>
+                    </center>
+                </form>
+                <section id="personal-info">
+                    <p>
+                        <i className="fas fa-phone"></i> +1 (516) 713 4111
+                    </p>
+                    <p>
+                        <i className="fas fa-envelope-square"></i> vcsahar@gmail.com
+                    </p> 
+                </section>
                     
-                    <div className="col-1"></div>
+                
+                <div className="col-1"></div>
                 
             </div>
         </main>
