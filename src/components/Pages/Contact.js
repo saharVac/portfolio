d64ae5edeba778b58a2984e377a70d699962b8a8
saharVac <vcsahar@gmail.com>
2020-12-28 17:24:34 -0500

    Integrated EmailJS to send me an email on form submittal

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useRef } from "react"
+import emailjs from "emailjs-com"
 
 function Contact() {
 
@@ -6,21 +7,19 @@ function Contact() {
     const emailRef = useRef()
     const messageRef = useRef()
 
-    const sendMessage = () => {
-        // check if all fields are filled
-        if (nameRef.current.value && emailRef.current.value && messageRef.current.value) {
-            const newMessage = {
-                to: "vcsahar@gmail.com",
-                subject: "PORTFOLIO CONTACT from: " + nameRef.current.value,
-                from: emailRef.current.value,
-                text: messageRef.current.value,
-                html: "<p>" + messageRef.current.value + "</p>"
-            }
-            console.log(newMessage)
-            alert("Message Sent!")
-        } else {
-            alert("Fill empty fields!")
-        }
+    const sendMail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_b67au6x', 'template_dgv034v', e.target, 'user_x9V1K4YqNlEX0TZAPB5BN')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+            .catch(err => console.log)
+
+
+        e.target.reset()
     }
      
     return (
@@ -29,14 +28,14 @@ function Contact() {
                
                 <h1>Contact Me</h1>
 
-                <form className="contact-form mt-4 pt-4" >
+                <form onSubmit={sendMail} className="contact-form mt-4 pt-4" >
                     <div className="form-group">
                         <label htmlFor="name">Full Name</label>
                         <input
                         ref={nameRef}
-                        type="email"
                         className="form-control"
                         id="name"
+                        name="name-from"
                         />
                     </div>
                     <div className="form-group">
@@ -47,6 +46,7 @@ function Contact() {
                         className="form-control"
                         id="email"
                         placeholder="name@example.com"
+                        name="email-from"
                         />
                     </div>
                     <div className="form-group">
@@ -56,18 +56,11 @@ function Contact() {
                         className="form-control"
                         id="exampleFormControlTextarea1"
                         rows="3"
+                        name="message"
                         ></textarea>
                     </div>
                     <center>
-                    <button 
-                        type="submit" 
-                        onClick={(e) => {
-                            e.preventDefault()
-                            sendMessage()
-                        }
-                    }>
-                        Submit
-                    </button>
+                    <button type="submit" >Submit</button>
                     </center>
 
                     <section id="personal-info">
